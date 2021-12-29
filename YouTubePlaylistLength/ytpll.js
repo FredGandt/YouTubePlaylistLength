8f73744997ad0b9cb1ff329c4b7d3b9bc611187e
Fred Gandt <fred.gandt.aka.fg@gmail.com>
2021-12-29 18:32:08 +0000

    fix whitespace

--- YouTubePlaylistLength/ytpll.js
+++ YouTubePlaylistLength/ytpll.js
@@ -3,27 +3,27 @@
 const output = document.createElement( "output" ),
 
 	seconds2Timestring = total_seconds => {
-  	let remaining_seconds = total_seconds % 60,
-      m = ( total_seconds - remaining_seconds ) / 60,
-      minutes = m % 60,
-      hours = ( m - minutes ) / 60;
-  	return [
-    	( hours ? `${hours}`.padStart( 2, "0" ) : "" ),
-    	`${minutes}`.padStart( 2, "0" ),
-    	`${Math.floor( remaining_seconds )}`.padStart( 2, "0" )
-  	].filter( i => i ).join( ":" );
+		let remaining_seconds = total_seconds % 60,
+			m = ( total_seconds - remaining_seconds ) / 60,
+			minutes = m % 60,
+			hours = ( m - minutes ) / 60;
+		return [
+			( hours ? `${hours}`.padStart( 2, "0" ) : "" ),
+			`${minutes}`.padStart( 2, "0" ),
+			`${Math.floor( remaining_seconds )}`.padStart( 2, "0" )
+		].filter( i => i ).join( ":" );
 	},
 
 	timestringArray2Seconds = arr => {
-  	return arr.reduce( ( a, timestring ) => {
-    	let ts = timestring.split( ":" ),
-        lo = 0, siah = 0;
-    	if ( ts.length === 3 ) {
-      	siah = parseInt( ts[ 0 ] ) * 3600;
-      	lo = 1;
-    	}
-    	return a + siah + ( parseInt( ts[ lo + 0 ] ) * 60 ) + parseInt( ts[ lo + 1 ] );
-  	}, 0 );
+		return arr.reduce( ( a, timestring ) => {
+			let ts = timestring.split( ":" ),
+				lo = 0, siah = 0;
+			if ( ts.length === 3 ) {
+				siah = parseInt( ts[ 0 ] ) * 3600;
+				lo = 1;
+			}
+			return a + siah + ( parseInt( ts[ lo + 0 ] ) * 60 ) + parseInt( ts[ lo + 1 ] );
+		}, 0 );
 	},
 
 	timestringArray2Timestring = arr => seconds2Timestring( timestringArray2Seconds( arr ) );
